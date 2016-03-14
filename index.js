7161baaac49de063b13c0b252a9673dc1e8f9d3a
James Costian <james@jamescostian.com>
2016-03-14 18:44:08 -0500

    Add some starter code

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-module.exports = function () {}
+let BorrowState = function (opts) {
+  this.unsafe = !!opts.unsafe
+  let state = {}
+  state.block = (r) => {
+    if (r === 'r') {
+      r = true
+    }
+  }
+  return this
+}
+
+module.exports = BorrowState

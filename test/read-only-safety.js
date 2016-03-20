5f8c9d518327b3969127d72decbc94185f4913ce
James Costian <james@jamescostian.com>
2016-03-19 19:20:11 -0500

    Make the test actually function

--- test/read-only-safety.js
+++ test/read-only-safety.js
@@ -9,4 +9,14 @@ test(`read-only-safety`, (t) => {
     state.foo = Math.PI
     state.unblock()
   })
+  myState.block('r').then((state) => {
+    t.ok(true)
+    state.foo = 5
+    state.unblock()
+  })
+  myState.block().then((state) => {
+    t.ok(true)
+    t.equal(state.foo, Math.PI)
+    state.unblock()
+  })
 })

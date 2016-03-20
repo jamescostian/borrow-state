0398caf24c31ce5c4391c6c534f32fc177489f5d
James Costian <james@jamescostian.com>
2016-03-20 18:04:53 -0500

    Make the test actually work

--- test/read-only-safety.js
+++ test/read-only-safety.js
@@ -2,21 +2,24 @@ const test = require('tape')
 const BorrowState = require('borrow-state')
 
 test(`read-only-safety`, (t) => {
-  t.plan(4)
+  t.plan(2)
+  let hasHappened = []
   let myState = new BorrowState()
   myState.block().then((state) => {
-    t.ok(true)
+    hasHappened.push(1)
     state.foo = Math.PI
     state.unblock()
   })
   myState.block('r').then((state) => {
-    t.ok(true)
+    hasHappened.push(2)
     state.foo = 5
     state.unblock()
   })
+  // The following is not set to read-only so that it can occur *after* the previous
   myState.block().then((state) => {
-    t.ok(true)
-    t.equal(state.foo, Math.PI)
+    hasHappened.push(3)
+    t.equal(state.foo, Math.PI, 'The state should still be set to pi')
+    t.equal(hasHappened, [1, 2, 3], 'The operations happened in the right order')
     state.unblock()
   })
 })

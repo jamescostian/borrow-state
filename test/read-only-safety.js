6f06a4828f997b12938b1a7129f8bfc6da5fdc96
James Costian <james@jamescostian.com>
2016-03-21 17:49:03 -0500

    Add the same test, but with promise chaining

--- test/read-only-safety.js
+++ test/read-only-safety.js
@@ -23,3 +23,24 @@ test(`read-only-safety`, (t) => {
     state.unblock()
   })
 })
+
+test(`read-only-safety, chained`, (t) => {
+  t.plan(2)
+  let hasHappened = []
+  let myState = new BorrowState()
+  myState.block().then((state) => {
+    hasHappened.push(1)
+    state.foo = Math.PI
+    state.unblock()
+  })
+  myState.block('r').then((state) => {
+    hasHappened.push(2)
+    state.foo = 5
+    return state
+  }).then((state) => {
+    hasHappened.push(3)
+    t.equal(state.foo, Math.PI, 'The state should still be set to pi')
+    t.equal(hasHappened, [1, 2, 3], 'The operations happened in the right order')
+    state.unblock()
+  })
+})

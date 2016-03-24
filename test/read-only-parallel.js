df06f09eb3ccdde7278a5f62c7809cbb10c1c8bd
James Costian <james@jamescostian.com>
2016-03-24 17:51:58 -0500

    Fix reference to lib in read-only-parallel

--- test/read-only-parallel.js
+++ test/read-only-parallel.js
@@ -1,5 +1,5 @@
 const test = require('tape')
-const BorrowState = require('borrow-state')
+const BorrowState = require('../lib/index.js')
 
 const sleep50ms = (something) => new Promise(function (resolve, reject) {
   setTimeout(function () {

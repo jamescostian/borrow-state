06026628919a73d348f0d6e987ac355443012134
James Costian <james@jamescostian.com>
2016-03-29 18:08:09 -0500

    Start working on strict-order test

---
+++ test/strict-order.js
@@ -0,0 +1,25 @@
+const test = require('tape')
+const BorrowState = require('../lib/module.js')
+
+test(`strict-order`, (t) => {
+  t.plan(2)
+  let hasHappened = []
+  let myState = new BorrowState()
+  myState.block().then((state) => {
+    hasHappened.push(1)
+    state.foo = Math.PI
+    state.unblock()
+  })
+  myState.block('r').then((state) => {
+    hasHappened.push(2)
+    state.foo = 5
+    state.unblock()
+  })
+  // The following is not set to read-only so that it can occur *after* the previous
+  myState.block().then((state) => {
+    hasHappened.push(3)
+    t.equal(state.foo, Math.PI, 'The state should still be set to pi')
+    t.equal(hasHappened, [1, 2, 3], 'The operations happened in the right order')
+    state.unblock()
+  })
+})

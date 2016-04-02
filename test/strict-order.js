98d54da94f1c8673c1c1c5e5574b98f807117f55
James Costian <james@jamescostian.com>
2016-04-02 17:48:55 -0500

    More tests

--- test/strict-order.js
+++ test/strict-order.js
@@ -14,11 +14,20 @@ test(`strict-order`, (t) => {
     return state
   }).then((state) => {
     hasHappened.push(3)
+    return state
+  }).then((state) => {
+    hasHappened.push(4)
+    return state
+  }).then((state) => {
+    hasHappened.push(5)
+    return state
+  }).then((state) => {
+    hasHappened.push(6)
     state.unblock()
   })
   myState.block().then((state) => {
-    hasHappened.push(4)
-    t.equal(hasHappened, [1, 2, 3, 4], 'The operations happened in the right order')
+    hasHappened.push(7)
+    t.equal(hasHappened, [1, 2, 3, 4, 5, 6, 7], 'The operations happened in the right order')
     state.unblock()
   })
 })

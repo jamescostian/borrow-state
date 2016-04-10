d8af7354997d855063c82f716eaa66eb2ff0b49a
James Costian <james@jamescostian.com>
2016-04-10 18:37:04 -0500

    Use count-to in read-only-parallel

--- test/read-only-parallel.js
+++ test/read-only-parallel.js
@@ -2,13 +2,14 @@ const test = require('tape')
 const BorrowState = require('../lib/module.js')
 
 const sleep50ms = require('./sleep.js')
+const countTo = require('./count-to.js')
 
 ;[true, false].forEach((unsafe) => {
   test(`read-only-parallel (${unsafe ? 'un' : ''}safe)`, (t) => {
     t.plan(3)
     t.timeoutAfter(100)
     let myState = new BorrowState()
-    ;[1, 2, 3].forEach(() => {
+    countTo(3).forEach(() => {
       myState.block('r')
         .then(sleep50ms)
         .then((state) => {

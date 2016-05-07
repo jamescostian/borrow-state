8a8576546755c5fb2a836cd50d806dc403336d5c
James Costian <james@jamescostian.com>
2016-05-07 14:25:51 -0500

    Use Node v6 and above
    Signed-off-by: James Costian <james@jamescostian.com>

--- test/read-only-safety.js
+++ test/read-only-safety.js
@@ -4,7 +4,7 @@ const BorrowState = require('../lib/module.js')
 
 const countTo = require('./count-to.js')
 
-test(`read-only-safety`, (t) => {
+test('read-only-safety', (t) => {
   t.plan(2)
   let hasHappened = []
   let myState = new BorrowState()
@@ -21,28 +21,7 @@ test(`read-only-safety`, (t) => {
   // The following is not set to read-only so that it can occur *after* the previous
   myState.block().then((state) => {
     hasHappened.push(3)
-    t.equal(state.foo, 4, 'The state should still be set to pi')
-    t.equal(hasHappened, countTo(3), 'The operations happened in the right order')
-    state.unblock()
-  })
-})
-
-test(`read-only-safety, chained`, (t) => {
-  t.plan(2)
-  let hasHappened = []
-  let myState = new BorrowState()
-  myState.block().then((state) => {
-    hasHappened.push(1)
-    state.foo = 4
-    state.unblock()
-  })
-  myState.block('r').then((state) => {
-    hasHappened.push(2)
-    state.foo = 5
-    return state
-  }).then((state) => {
-    hasHappened.push(3)
-    t.equal(state.foo, 4, 'The state should still be set to pi')
+    t.equal(state.foo, 4, 'The state should still be unchanged by the read-only operation')
     t.deepEqual(hasHappened, countTo(3), 'The operations happened in the right order')
     state.unblock()
   })

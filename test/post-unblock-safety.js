2b91457a683398369551e43e6adfbf8a50b80856
James Costian <james@jamescostian.com>
2016-12-11 18:59:19 -0600

    Note that unsafe mode allows multiple unblock()s

--- test/post-unblock-safety.js
+++ test/post-unblock-safety.js
@@ -2,7 +2,7 @@
 const test = require('tape')
 const BorrowState = require('../lib/module.js')
 
-test('post-unblock-safety (safe)', (t) => {
+test('post-unblock-safety', (t) => {
   t.plan(4)
   let myState = new BorrowState()
   myState.block().then((state) => {
@@ -28,15 +28,5 @@ test('post-unblock-safety (safe)', (t) => {
     t.equal(state.bar, 5, 'Write operations do not permit mutations after state.block()')
     t.equal(state.foo, 5, 'Read-only operations do not permit mutations before/after state.block()')
     state.unblock()
-  })
-})
-
-test('post-unblock-safety (unsafe)', (t) => {
-  t.plan(2)
-  let myState = new BorrowState({unsafe: true})
-  myState.block().then((state) => {
-    state.unblock()
-    t.throws(state.unblock, 'state.unblock() doesn\'t work the second time you call it')
-    t.throws(state.unblock, 'state.unblock() doesn\'t work the third time you call it')
   })
 })
--- test/sleep.js
+++ test/sleep.js
@@ -1,8 +1,23 @@
 'use strict'
-module.exports = (time) => {
-  return (something) => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => resolve(something), 50)
+// Normally, sleep code would look like this:
+//   const sleep = time => new Promise(resolve => {
+//     setTimeout(() => resolve(), time)
+//   })
+// Then you could use that code like so: sleep(50).then(doStuff)
+// However, what if you want to use that sleep function with this module?
+// For example, imagine the following:
+//   myState.block('r')
+//     .then(sleep(50))
+//     .then((state) => { /* do something with the state */ })
+// Of course, it doesn't make sense to write code like that. Unless you're trying to test this module!
+// If you're trying to test this module, then the above code is great for testing parallelism.
+// In order for that code to work though, you have to make the sleep function return what it got passed.
+// That's why the implementation you see below is so... strange
+
+module.exports = time => {
+  return state => {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(state), 50)
     })
   }
 }

a37cb3c7455c76d09875ffbeb47b8f40e3b6c3bc
James Costian <james@jamescostian.com>
2019-01-29 15:57:40 -0600

    Fix lint errors

--- tests/custom-initial.test.js
+++ tests/custom-initial.test.js
@@ -2,8 +2,8 @@
 const BorrowState = require('../module.js')
 
 it('allows you to initialize it with any object', () => {
-  let myState0 = new BorrowState({initial: {foo: 5}})
-  let myState1 = new BorrowState({initial: [6]})
+  let myState0 = new BorrowState({ initial: { foo: 5 } })
+  let myState1 = new BorrowState({ initial: [6] })
   return Promise.all([myState0.block(), myState1.block()]).then(states => {
     expect(states[0].foo).toEqual(5)
     expect(states[1][0]).toEqual(6)
